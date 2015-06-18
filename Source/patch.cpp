ad6670dca5a1d26559f2bc670410d2b0dd98b0d5
Joshua Freedman <npjoshf@gmail.com>
2015-06-18 13:23:32 -0700

    Made windows compatibale due to mingw bug - PATCH

---
+++ Source/patch.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Joshua on 6/18/2015.
+//
+
+#include "patch.h"
+
+#include <string>
+#include <sstream>
+
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
---
+++ Source/patch.h
@@ -0,0 +1,8 @@
+//
+// Created by Joshua on 6/18/2015.
+//
+
+#ifndef LAST_STAND_ENGINE_PATCH_H
+#define LAST_STAND_ENGINE_PATCH_H
+
+#endif //LAST_STAND_ENGINE_PATCH_H
